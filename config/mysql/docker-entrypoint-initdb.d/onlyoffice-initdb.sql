6b4e29575c080737fa085903ae86b4c276081e3c
zengxc <zxc916684@163.com>
2021-03-26 15:43:00 +0800

    update compose file

--- config/mysql/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
+++ config/mysql/docker-entrypoint-initdb.d/onlyoffice-initdb.sql
@@ -1,13 +1,4 @@
 CREATE DATABASE IF NOT EXISTS onlyoffice CHARACTER SET "utf8" COLLATE "utf8_general_ci";
-CREATE DATABASE IF NOT EXISTS onlyoffice_mailserver CHARACTER SET "utf8" COLLATE "utf8_general_ci";
-
-CREATE USER IF NOT EXISTS 'onlyoffice_user'@'%' IDENTIFIED BY '123456';
-CREATE USER IF NOT EXISTS 'onlyoffice_mailserver_user'@'%' IDENTIFIED BY '123456';
-
-GRANT ALL PRIVILEGES ON onlyoffice.* TO 'onlyoffice_user'@'%';
-GRANT ALL PRIVILEGES ON onlyoffice_mailserver.* TO 'onlyoffice_user'@'%';
-
-GRANT ALL PRIVILEGES ON onlyoffice.* TO 'onlyoffice_mailserver_user'@'%';
-GRANT ALL PRIVILEGES ON onlyoffice_mailserver.* TO 'onlyoffice_mailserver_user'@'%';
-
+CREATE USER IF NOT EXISTS 'onlyoffice'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON onlyoffice.* TO 'onlyoffice'@'%';
 FLUSH PRIVILEGES;
